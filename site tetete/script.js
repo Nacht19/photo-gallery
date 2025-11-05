01f0ebbcdc3b9ed46accee974ceb2a7a3a61f98e
Nacht19 <gabriel_he_moraes@estudante.sesisenai.org.br>
2025-11-05 16:57:24 -0300

    Add files via upload

---
+++ site tetete/script.js
@@ -0,0 +1,62 @@
+     //alert('script externo funcionando!')
+
+    //let nome = "Nacht"; // texto
+      //let idade = 19 // número
+      //const PI = 3.14 // constante
+
+     // let soma = 10 + 5;
+     // let subtracao = 10 - 3;
+     // let multiplicacao = 4 * 2;
+     // let divisao = 20 / 4;
+
+      //alert(nome + ' - ' + idade + ' - ' + PI + ' - ' + soma + ' - ' + subtracao + ' - ' + multiplicacao + ' - ' + divisao);
+
+      //let nome = prompt("Qual é o seu nome?")
+      //console.log("Bem-vindo, " + nome + "!");
+
+      //let idade = prompt("Qual sua idade ?");
+      //alert("você tem " + idade + " anos!");
+
+      //let n1 = Number(prompt("Digite um número:"));
+      //let n2 = Number(prompt("Digite outro número:"));
+      //let soma = n1 + n2;
+      //alert("A soma é: " + soma);
+
+      //let idade = Number(prompt("Digite sua idsde:"));
+      //if (idade >= 18) {
+        //alert("Você é maior de idade!");
+      ///} else {
+        //alert("você é menor de idade!");      
+    //}
+
+         //<---- Exe 1 ---->//
+
+    //   alert('Olá nome seja bem-vindora ao javaScript!')
+
+    //      //<---- Exe 2 ----->//
+    //   let num1 = Number(prompt('digite um número'));
+    //   let multiplicacao = num1 * 2;
+    //   alert ("o dobro do número é "+ multiplicacao);
+
+    //      //<---- Exe 3 ----->//
+    //   let n1 = Number(prompt("Digite um número:"));
+    //   let n2 = Number(prompt("Digite outro número:"));
+    //   let n3 = Number(prompt("Digite outro número:"));
+    //   let soma = n1 + n2 + n3;
+    //   let divisao = soma/3 
+    //   alert("A divisao é: " + divisao);
+    //   if (divisao >=7) {
+    //     alert('Aluno Aprovado')
+    //   } else {
+    //     alert ('Aluno Reprovado')
+    //   }
+         //<---- Exe 4 ----->//
+       alert();
+       let num = prompt('digite um número');
+       if(num %2 == 0){
+        alert("Par")
+       }else{
+        alert('IMPAR')
+       }
+
+       
